80ed90f0b0c540c1c3dde65d98acf6636f3bcee2
Mohamad Zorkot <zrktmhamad@gmail.com>
2025-09-17 23:56:09 +0300

    Finalize user migration changes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('phone')->nullable();
             $table->string('location')->nullable();
-            $table->enum('role', ['user', 'admin', 'moderator'])->default('user');
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
